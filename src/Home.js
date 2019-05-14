4da2a6903fcf51e21a22ca0a6ed42c95a14c0d14
ventuks26 <mateventura@gmail.com>
2019-05-14 09:08:33 -0500

    Cree columnas en Home

--- src/Home.js
+++ src/Home.js
@@ -4,10 +4,14 @@ import logo from "./img/logo.png";
 class Home extends Component {
     render(){
         return(
-        <div className="backgroundHome"> 
+    <div className="backgroundHome">
+        <div className="col-sm-6">
             <img className="logo" src={logo} />        
+        </div>
+        <div className=" col-sm-offset-6"> 
             <button type="button" className="start">Inicio</button>
         </div>
+    </div>
    
         );
     }
