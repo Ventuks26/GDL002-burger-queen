5e06daceb734bced248fb99624779296b9db03c5
Ventuks26 <mateventura5@gmail.com>
2019-05-28 08:12:46 -0500

    Updates

--- precache-manifest.de2eed295080c3c2bb3686167de12da3.js
+++ precache-manifest.de2eed295080c3c2bb3686167de12da3.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7a31e6a6e1337fedbd1f292b25f24612",
+    "revision": "7bd855e35ece5c7b406da3766ded17ad",
     "url": "/GDL002-burger-queen/index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/GDL002-burger-queen/static/css/2.22a7d4ef.chunk.css"
   },
   {
-    "revision": "50ebf6a36f069b37bf4b",
-    "url": "/GDL002-burger-queen/static/css/main.4bf82d64.chunk.css"
+    "revision": "457e4a2846ec2b10a5e1",
+    "url": "/GDL002-burger-queen/static/css/main.7b73001f.chunk.css"
   },
   {
     "revision": "06c9bd1d4437989f30be",
     "url": "/GDL002-burger-queen/static/js/2.0eda45d0.chunk.js"
   },
   {
-    "revision": "50ebf6a36f069b37bf4b",
-    "url": "/GDL002-burger-queen/static/js/main.c40d9768.chunk.js"
+    "revision": "457e4a2846ec2b10a5e1",
+    "url": "/GDL002-burger-queen/static/js/main.bcf3769e.chunk.js"
   },
   {
     "revision": "f8c6079179fe30af807c",
